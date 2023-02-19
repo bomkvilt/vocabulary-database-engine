f124ab3936912a98fa63a944e9d84c94c0f84ba1
bomkvilt <kvilt-d.max@yandex.ru>
2023-02-19 16:32:58 +0300

    Adaptation to a git submodule use case: - injection to a python path - __main__.py example fix

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,4 +1,10 @@
-from .engine import Engine
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+from .engine import Engine  # noqa: E402
 
 __all__ = [
     "Engine",
